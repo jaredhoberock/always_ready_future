049fb15d2f16e1e472c18a7f0158b0c2ac61256a
Jared Hoberock <jaredhoberock@gmail.com>
2018-04-04 11:39:27 -0500

    Import more implementation of always_ready_future from Agency

--- always_ready_future.hpp
+++ always_ready_future.hpp
@@ -1,5 +1,33 @@
+// Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include <exception>
 #include <type_traits>
+#include <utility>
+#include <future>
 #include "variant.hpp" // my compiler doesn't have <variant> yet
 #include "optional.hpp" // my compiler doesn't have <optional> yet
 
@@ -8,63 +36,96 @@
 // (whatever that may be someday)
 //
 // Executors which always block their client can use always_ready_future as their
-// associated future and still expose two-way asynchronous execution functions like async_execute()
+// associated future and still expose two-way asynchronous execution functions like .twoway_execute()
 
 template<class T>
 class always_ready_future
 {
   public:
-    always_ready_future(const T& value) : value_(value) {}
+    // Default constructor creates an invalid always_ready_future
+    // Postcondition: !valid()
+    always_ready_future() = default;
 
-    always_ready_future(T&& value) : value_(std::move(value)) {}
+    always_ready_future(const T& value) : state_(value) {}
 
-    always_ready_future(std::exception_ptr e) : value_(e) {}
+    always_ready_future(T&& value) : state_(std::move(value)) {}
+
+    always_ready_future(std::exception_ptr e) : state_(e) {}
+
+    always_ready_future(always_ready_future&& other)
+      : state_()
+    {
+      using std::swap;
+      swap(state_, other.state_);
+    }
+
+    always_ready_future& operator=(always_ready_future&& other)
+    {
+      state_.reset();
+
+      using std::swap;
+      swap(state_, other.state_);
+
+      return *this;
+    }
+
+    constexpr static bool is_ready()
+    {
+      return true;
+    }
 
   private:
-    struct get_visitor
+    struct get_ptr_visitor
     {
-      T operator()(T& value) const
+      T* operator()(T& result) const
       {
-        return std::move(value);
+        return &result;
       }
 
-      T operator()(std::exception_ptr& e) const
+      T* operator()(std::exception_ptr& e) const
       {
-        throw e;
-
-        // XXX rework this visitor to avoid returning T in both cases
-        return T();
+        std::rethrow_exception(e);
+        return nullptr;
       }
     };
+
+    T& get_ref()
+    {
+      if(!valid())
+      {
+        throw std::future_error(std::future_errc::no_state);
+      }
+
+      return *std::experimental::visit(get_ptr_visitor(), *state_);
+    }
 
   public:
     T get()
     {
-      std::experimental::visit(get_visitor(), value_);
+      T result = std::move(get_ref());
+
+      invalidate();
+
+      return result;
     }
 
-    // arguably, waiting on an always_ready_future might indicate
-    // a programmer error, but this type is intended to be used in
-    // generic code where the concrete type of Future is not known in advance
     void wait() const
     {
-      // wait() is a no-op: this is always ready
+      // wait() is a no-op: *this is always ready
     }
 
-    // there is no valid() to avoid having to store an extra bit
-    // get() does not invalidate this future, but it does move its value
-    // XXX should get() also set value_ to a future_error?
-    
-    // no share(), no then(), but there's no reason there couldn't be other than
-    // implementation inconvenience
+    bool valid() const
+    {
+      return state_.has_value();
+    }
 
   private:
-    // XXX we could avoid the overhead of this variant if
-    //     we didn't allow this type to have an exceptional case
-    //     the problem with that design is that this type is
-    //     intended to be used in generic code where the concrete type of Future is not known
-    //     and the way that generic clients interact with exceptional cases ought to be uniform
-    std::experimental::variant<T,std::exception_ptr> value_;
+    void invalidate()
+    {
+      state_ = std::experimental::nullopt;
+    }
+
+    std::experimental::optional<std::experimental::variant<T,std::exception_ptr>> state_;
 };
 
 
@@ -72,42 +133,94 @@ template<>
 class always_ready_future<void>
 {
   public:
-    always_ready_future() {}
+    // XXX the default constructor creates a ready, valid future, but we may wish
+    //     to redefine the default constructor to create an invalid future
+    //     in such a scheme, we would need to distinguish another constructor for
+    //     creating a ready (void) result.
+    //     We could create an emplacing constructor distinguished with an in_place_t parameter
+    always_ready_future() : valid_(true) {}
 
-    always_ready_future(std::exception_ptr e) : exception_(e) {}
+    always_ready_future(std::exception_ptr e) : exception_(e), valid_(true) {}
+
+    always_ready_future(always_ready_future&& other)
+      : exception_(), valid_(false)
+    {
+      using std::swap;
+      swap(exception_, other.exception_);
+      swap(valid_, other.valid_);
+    }
+
+    always_ready_future& operator=(always_ready_future&& other)
+    {
+      exception_.reset();
+      valid_ = false;
+
+      using std::swap;
+      swap(exception_, other.exception_);
+      swap(valid_, other.valid_);
+      return *this;
+    }
+
+    constexpr static bool is_ready()
+    {
+      return true;
+    }
 
   public:
     void get()
     {
+      if(!valid())
+      {
+        throw std::future_error(std::future_errc::no_state);
+      }
+
+      invalidate();
+
       if(exception_)
       {
-        throw exception_.value();
+        std::rethrow_exception(exception_.value());
       }
     }
 
-    // arguably, waiting on an always_ready_future might indicate
-    // a programmer error, but this type is intended to be used in
-    // generic code where the concrete type of Future is not known in advance
     void wait() const
     {
       // wait() is a no-op: this is always ready
     }
 
-    // there is no valid() to avoid having to store an extra bit
-    // get() does not invalidate this future, but it does move its value
-    // XXX should get() also set exception_ to a future_error?
-    
-    // no share(), no then(), but there's no reason there couldn't be other than
-    // implementation inconvenience
+    bool valid() const
+    {
+      return valid_;
+    }
 
   private:
-    // XXX we could avoid the overhead of this optional if
-    //     we didn't allow this type to have an exceptional case
-    //     the problem with that design is that this type is
-    //     intended to be used in generic code where the concrete type of Future is not known
-    //     and the way that generic clients interact with exceptional cases ought to be uniform
+    void invalidate()
+    {
+      valid_ = false;
+    }
+
     std::experimental::optional<std::exception_ptr> exception_;
+
+    bool valid_;
 };
+
+
+template<class T>
+always_ready_future<std::decay_t<T>> make_always_ready_future(T&& value)
+{
+  return always_ready_future<std::decay_t<T>>(std::forward<T>(value));
+}
+
+inline always_ready_future<void> make_always_ready_future()
+{
+  return always_ready_future<void>();
+}
+
+
+template<class T>
+always_ready_future<T> make_always_ready_exceptional_future(std::exception_ptr e)
+{
+  return always_ready_future<T>(e);
+}
 
 
 namespace detail
